917f22b3a558f9560033992af9e7acf70059d298
faris <faris@mail.com>
2021-11-10 06:54:13 -0800

    component: create header component :art:

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import { Container, Navbar } from "react-bootstrap";
+
+const Header = (props) => {
+  return (
+    <Navbar as="header" bg="light">
+      <Container>
+        <Navbar.Brand href="/">
+          <h3>
+            Binary Search <span className="text-primary">Visualizer</span>
+          </h3>
+        </Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
