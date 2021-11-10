fcce2986313ed400f2027e352fed118642b4e3a5
faris <faris@mail.com>
2021-11-10 07:42:38 -0800

    style: add footer component :sparkles:

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import { Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 function App() {
   return (
-    <Container fluid className="p-0">
-      <Header />
+    <Container fluid>
+      <Row xs={1} className="d-flex flex-column min-vh-100">
+        <Col className="p-0">
+          <Header />
+        </Col>
+        <Col>main</Col>
+        <Col className="p-0 mt-auto">
+          <Footer />
+        </Col>
+      </Row>
     </Container>
   );
 }
