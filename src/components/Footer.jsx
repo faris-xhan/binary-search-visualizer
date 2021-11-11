9c472566bf04771e34840b119fccef11eb46b41f
faris <faris@mail.com>
2021-11-10 18:32:29 -0800

    updae foot layout and content :fire:

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,29 +1,21 @@
+import Heart from "./Heart";
 const Footer = (props) => {
   return (
     <footer className="footer mt-auto py-3 bg-light">
-      <div className="d-flex justify-content-center">
+      <div className="d-flex justify-content-center flex-wrap">
         <span className="text-muted">
           Copyright &copy; {new Date().getFullYear()}
         </span>
         &nbsp;
-        <span className="text-muted">
-          <p>
-            Created with &nbsp;
-            <svg
-              viewBox="0 0 512 512"
-              width="14"
-              height="14"
-              aria-label="love"
-              class="inline-block"
-            >
-              <path
-                fill="#e31b23"
-                d="M462.3 62.6C407.5 15.9 326 24.3 275.7 76.2L256 96.5l-19.7-20.3C186.1 24.3 104.5 15.9 49.7 62.6c-62.8 53.6-66.1 149.8-9.9 207.9l193.5 199.8c12.5 12.9 32.8 12.9 45.3 0l193.5-199.8c56.3-58.1 53-154.3-9.8-207.9z"
-              ></path>
-            </svg>{" "}
+        <div className="text-muted d-flex align-items-center justify-content-center flex-wrap">
+          Created with &nbsp;
+          <Heart /> &nbsp;
+          <div className="d-inline-block">
             by <a href="https://twitter.com/nemoxhan">Nemo</a>
-          </p>
-        </span>
+            &nbsp; using <a href="https://reactjs.org/">React.js</a> and{" "}
+            <a href="https://react-bootstrap.github.io/">React Bootstrap</a>
+          </div>
+        </div>
       </div>
     </footer>
   );
